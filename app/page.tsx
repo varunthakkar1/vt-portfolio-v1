9744325a8724eb7928539960b0e703c155c9158c
varunthakkar1 <varunthakkar2013@gmail.com>
2023-08-18 20:14:14 -0400

    finished star background

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,25 @@
 import Image from 'next/image'
-const MAX_STAR_DELAY: number = 500
-const MAX_STARS: number = 500
+const MAX_STAR_DELAY: number = 2000
+const MAX_STARS: number = 200
 
-const generateStar1 = (): JSX.Element => {
-  const delay: number = Math.floor(Math.random() * MAX_STAR_DELAY)
-  const top: string = String(Math.floor(Math.random() * 500))
-  const pos: string = `${top}px`
-  const className = `rounded-full absolute top-[200px] left-[200px] w-[4px] h-[4px] bg-white animate-twinkle delay-[${delay}ms]`
-  console.log(className)
-  return <div className={className}/>
+const generateStar = (): JSX.Element => {
+  const delay: number = Math.floor(Math.random() * MAX_STAR_DELAY) - 1000
+  const top: number = Math.floor(Math.random() * 100)
+  const left: number = Math.floor(Math.random() * 100)
+  const delayString: string = `${delay}ms`
+  const className = `rounded-full absolute w-[3px] h-[3px] bg-white animate-twinkle`
+  return <div className={className} style={{ top: `${top}vh`, left: `${left}vw`, animationDelay: delayString}}/>
 }
-
-const generateStar2 = (): JSX.Element => {
-  const delay: number = Math.floor(Math.random() * MAX_STAR_DELAY)
-  const top: string = String(Math.floor(Math.random() * 500))
-  const pos: string = `${top}px`
-  const className = `rounded-full absolute top-[500px] left-[500px] w-[4px] h-[4px] bg-white animate-twinkle delay-[${delay}ms]`
-  console.log(className)
-  return <div className={className}/>
-}
-
-const stars = new Array<number>(5);
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className='flex bg-primary w-full h-screen justify-center items-center'>
-        <div className='flex text-secondary text-8xl font-medium font-montserrat'>Varun Thakkar</div>
-        {generateStar1()}
-        {generateStar2()}
+    <main className="flex min-h-screen flex-col items-center justify-between font-montserrat">
+      {new Array<number>(MAX_STARS).fill(0).map((key) => {
+          return <div key={key}>{generateStar()}</div>
+        })}
+      <div className='flex flex-col bg-primary w-full h-screen justify-center items-center'>
+        <div className='flex text-white text-8xl font-medium z-10 w-full justify-center'>Varun Thakkar</div>
+        <div className='flex text-secondary text-4xl font-normal z-10 w-full justify-center'>Software Engineer</div>
       </div>
     </main>
   )
