43392c478c650023f264801ae8056b2ddf105ffb
varunthakkar1 <varunthakkar2013@gmail.com>
2023-09-02 13:08:05 -0700

    adding transitions

--- app/components/about/About.tsx
+++ app/components/about/About.tsx
@@ -1,10 +1,24 @@
+"use client";
+import { useInView } from "react-intersection-observer";
+
 const About = (): JSX.Element => {
+  const { ref, inView, entry } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  });
+
+  const style = { opacity: inView ? "100" : "0" };
+
   return (
-    <div className="flex flex-col w-full min-h-screen justify-center items-center">
+    <div
+      ref={ref}
+      className="flex flex-col w-full min-h-screen justify-center items-center transition-opacity ease-in duration-[2s] opacity-0"
+      style={style}
+    >
       <div className="flex w-full justify-center text-white text-lg md:text-2xl lg:text-4xl">
         ABOUT ME
       </div>
-      <div className="flex justify-center w-[70%] items-center text-slate-400 text-md md:text-xl lg:text-2xl">
+      <div className="flex justify-center min-w-[300px] max-w-[50%] items-center text-slate-300 text-md md:text-xl lg:text-2xl">
         In May 2023, I graduated with magna cum laude from Northeastern
         University with a BS in Computer Science.
       </div>
--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -5,21 +5,29 @@ import { useInView } from "react-intersection-observer";
 const Hero = (): JSX.Element => {
   const { ref, inView, entry } = useInView({
     threshold: 0,
+    triggerOnce: true,
   });
 
+  const style = { opacity: inView ? "100" : "0" };
+
   return (
-    <div
-      ref={ref}
-      style={{ opacity: inView ? "100" : "0" }}
-      className="flex flex-col w-full min-h-screen justify-center transition-opacity ease-in duration-700 opacity-0"
-    >
-      <div className="flex text-white text-2xl md:text-4xl lg:text-8xl font-medium z-10 w-full justify-center align-middle">
+    <div ref={ref} className="flex flex-col w-full min-h-screen justify-center">
+      <div
+        style={style}
+        className="flex text-white text-2xl md:text-4xl lg:text-8xl font-medium z-10 w-full justify-center align-middle transition-opacity ease-in duration-[1s] opacity-0"
+      >
         VARUN THAKKAR
       </div>
-      <div className="flex text-slate-300 text-xl md:text-2xl lg:text-4xl font-normal z-10 w-full justify-center">
+      <div
+        style={style}
+        className="flex text-slate-300 text-xl md:text-2xl lg:text-4xl font-normal z-10 w-full justify-center transition-opacity ease-in duration-[1s] opacity-0 delay-[1s]"
+      >
         SOFTWARE ENGINEER
       </div>
-      <div className="relative text-white w-full top-[40vh] animate-bounce">
+      <div
+        style={style}
+        className="relative text-white w-full top-[40vh] animate-bounce transition-opacity ease-in duration-[1s] opacity-0 delay-[2s]"
+      >
         <div className="flex justify-center">
           <AiOutlineArrowDown size={"50px"} />
         </div>
